cd642fd69d2345d1d88c22be408f8048c219a59d
davidbison <davidbisonhubert@gmail.com>
2016-07-08 22:02:06 -0700

    Initial commit

---
+++ comic-release-dates-spec.rb
@@ -0,0 +1,2 @@
+require_relative 'comic-release-dates'
+
---
+++ comic-release-dates.rb
@@ -0,0 +1,122 @@
+require 'nokogiri'
+require 'open-uri'
+require 'date'
+# stores month conversion hashes
+require_relative 'month_conv'
+
+
+# class names for locating info while scraping
+COMIC_NAME = 'p.book__headline'
+RELEASE_DATE = 'p.book__text'
+
+
+# storing pages as Nokogiri objects, then creating a list out of them
+AUTUMNLANDS = Nokogiri::HTML(open('https://imagecomics.com/comics/series/tooth-and-claw'))
+DESCENDER = Nokogiri::HTML(open('https://imagecomics.com/comics/series/descender'))
+GODDAMNED = Nokogiri::HTML(open('https://imagecomics.com/comics/series/the-goddamned'))
+INJECTION = Nokogiri::HTML(open('https://imagecomics.com/comics/series/injection'))
+PAPER_GIRLS = Nokogiri::HTML(open('https://imagecomics.com/comics/series/paper-girls'))
+SAGA = Nokogiri::HTML(open('https://imagecomics.com/comics/series/saga'))
+SOUTHERN_BASTARDS = Nokogiri::HTML(open('https://imagecomics.com/comics/series/southern-bastards'))
+PAGES = [AUTUMNLANDS, DESCENDER, GODDAMNED, INJECTION, PAPER_GIRLS, SAGA, SOUTHERN_BASTARDS]
+
+
+# helper function to format release date as Date obj
+def format_date_obj(calendar, title, date)
+  month = 0
+  day = 0
+  year = 0
+  split_date = date.text.split(' ')
+  if split_date.include?('Published:')
+    split_date.shift
+  end
+
+  month += MONTH_AS_INT[split_date[0]]
+  day += split_date[1].to_i
+  year += split_date[2].to_i
+  calendar[title.text] = Date.new(year, month, day)
+end
+
+
+# helper function to format release date for print view
+def format_date_print_view(date)
+  puts "#{MONTH_AS_ROM[date.month]} #{date.mday}, #{date.year}\n\n"
+end
+
+
+# pull info from pages and store in list
+def record_releases(pages)
+  releases = {}
+  pages.each do |series|
+    issues = series.css(COMIC_NAME)
+    release_dates = series.css(RELEASE_DATE)
+    issues.each_with_index do |title, index|
+      format_date_obj(releases, title, release_dates[index])
+    end
+  end
+  return releases
+end
+
+
+# print all comics for a specific series
+def list_all(series)
+
+end
+
+
+# old method to print releases
+def old_print_releases(calendar, month)
+  calendar.each do |title, release_date|
+    if release_date.mon == month && release_date.year >= Time.now.to_date.year
+      print "#{title}\n"
+      format_date_print_view(release_date)
+    end
+  end
+end
+
+
+# collect releases by month
+def monthly_releases(calendar, month)
+  monthly_releases = {}
+  calendar.each do |title, release_date|
+    if release_date.mon == month && release_date.year >= Time.now.to_date.year
+      monthly_releases[title] = release_date
+      print "#{title}\n"
+      format_date_print_view(release_date)
+    end
+  end
+  return monthly_releases
+end
+
+
+# sort releases by date
+def sort_by_date(comics)
+
+end
+
+
+# print new issues by release date
+def print_list(comics)
+
+end
+
+
+# user input sets which month to show releases for
+def user_input(releases)
+  puts 'What month do you want to see releases for?'
+  input = gets.chomp.to_i
+  old_print_releases(releases, input)
+  # comics = monthly_releases(releases, input)
+  # sorted_comics = sort_by_date(comics)
+  # print_list(sorted_comics)
+end
+
+
+# cost of comics $2.99
+# tax = $0.52
+# total per comic = $3.51
+
+
+# driver code
+releases = record_releases(PAGES)
+user_input(releases)
