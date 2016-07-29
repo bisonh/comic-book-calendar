98806795c3e6d65b3b2d0dc413841f9d7d3c2551
davidbison <davidbisonhubert@gmail.com>
2016-07-28 21:17:49 -0700

    Organize helper files and rename page

--- helpers/comic-pages.rb
+++ helpers/comic-pages.rb
@@ -1,6 +1,8 @@
 require 'nokogiri'
 require 'open-uri'
 
+
+# scraped HTML from individual series pages
 AUTUMNLANDS = Nokogiri::HTML(open('https://imagecomics.com/comics/series/tooth-and-claw'))
 DESCENDER = Nokogiri::HTML(open('https://imagecomics.com/comics/series/descender'))
 GODDAMNED = Nokogiri::HTML(open('https://imagecomics.com/comics/series/the-goddamned'))
@@ -9,4 +11,11 @@ PAPER_GIRLS = Nokogiri::HTML(open('https://imagecomics.com/comics/series/paper-g
 SAGA = Nokogiri::HTML(open('https://imagecomics.com/comics/series/saga'))
 SOUTHERN_BASTARDS = Nokogiri::HTML(open('https://imagecomics.com/comics/series/southern-bastards'))
 
+
+# collection of scraped pages
 PAGES = [AUTUMNLANDS, DESCENDER, GODDAMNED, INJECTION, PAPER_GIRLS, SAGA, SOUTHERN_BASTARDS]
+
+
+# class names for locating info while scraping
+COMIC_NAME_NODE = 'p.book__headline'
+RELEASE_DATE_NODE = 'p.book__text'
