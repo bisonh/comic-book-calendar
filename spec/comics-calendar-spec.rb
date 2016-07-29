891e1c7f0f4ec8e22c7e9469572d6a6c0fe291f4
davidbison <davidbisonhubert@gmail.com>
2016-07-28 21:09:40 -0700

    Add tests for adding comics to the list and filtering out collections

--- spec/comics-calendar-spec.rb
+++ spec/comics-calendar-spec.rb
@@ -3,41 +3,23 @@ require_relative './comics-objects'
 
 
 # test data
-# array of two comics objects
+pages = [AUTUMNLANDS, DESCENDER]
+comics_list = comics_and_release_dates(pages)
 
 
-describe 'all_comics' do
+describe 'comics_and_release_dates' do
   it 'returns a hash' do
-    expect(all_comics(comics)).to be_a_kind_of(Hash)
+    expect(comics_list).to be_a_kind_of(Hash)
   end
 
-  it 'is not empty an empty list' do
-    expect(all_comics(comics)).not_to be_empty
+  it 'the hash is not empty' do
+    expect(comics_list).not_to be_empty
   end
 end
 
+describe 'filter_out_collections' do
+  it 'returns a hash' do
+    expect(filter_out_collections(comics_list)).to be_a_kind_of(Hash)
+  end
 
-# describe 'single_issues_only_filter' do
-#   it 'returns a hash' do
-#     expect(single_issues_only_filter(comics).to be_a_kind_of(Hash))
-#   end
-
-#   it 'is not empty an empty list' do
-#     expect(single_issues_only_filter(comics)).not_to be_empty
-#   end
-
-#   it "filters out keys with 'vol' in the title" do
-#     expect(single_issues_only_filter(comics)).not_to include('vol')
-#   end
-
-#   it "filters out keys with ':' in the title" do
-#     expect(single_issues_only_filter(comics)).not_to include(':')
-#   end
-# end
-
-
-# # describe 'format_date_obj' do
-# #   it 'returns a date object' do
-
-# #   end
-# # end
+end
