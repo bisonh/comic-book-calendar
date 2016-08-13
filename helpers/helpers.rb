ea329f9fac0ec16522125904ac5f67cf89c3bd75
davidbison <davidbisonhubert@gmail.com>
2016-08-12 17:15:24 -0700

    Replace single issue filter method with boolean output

--- helpers/helpers.rb
+++ helpers/helpers.rb
@@ -7,15 +7,8 @@ end
 
 
 # filter out volumes and special editions
-# output: { "saga #30" => "Published: January 8, 2016" }
-def remove_collections(comics)
-  single_issues = {}
-  comics.each do |title, release_date|
-    if title.include?('#') && !title.include?('free #1')
-      if !title.downcase.include?('vol') && !title.include?(':')
-        single_issues[title] = release_date
-      end
-    end
+def single_issue?(title)
+  if title.include?('#') && !title.downcase.include?('free #1')
+    return !title.downcase.include?('vol') && !title.include?(':')
   end
-  return single_issues
 end
